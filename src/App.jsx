364c68cecce533ca1c36bd59227a1db52f4c6e55
Jozef Piecyk <jozollo@gmail.com>
2019-03-17 12:33:58 +0100

    add client query

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
 });
 
-client
-  .query({
-    query: gql`
-      {
-        books {
-          title
-          author
-        }
-      }
-    `,
-  })
-  .then(data => console.log(data));
+const GET_BOOKS = gql`
+  {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+const BooksList = () => (
+  <Query query={GET_BOOKS}>
+    {({ loading, error, data, refetch }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>title</th>
+              <th>author</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.books.map(book => (
+              <tr key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }}
+  </Query>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Info w konsoli</p>
-        </header>
+        <ApolloProvider client={client}>
+          <BooksList />
+        </ApolloProvider>
       </div>
     );
   }
